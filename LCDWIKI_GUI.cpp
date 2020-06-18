53df20fe9931fd8ea0063791dac10f3deec5c0e4
phorton1 <available@upon.request>
2020-06-18 16:11:19 -0500

    added dlgFtpTuner

--- LCDWIKI_GUI.cpp
+++ LCDWIKI_GUI.cpp
@@ -1226,7 +1226,7 @@ void LCDWIKI_GUI::printf_justified(
 
 
 
-void LCDWIKI_GUI:drawBorderedRectangle(int x, int y, int w, int h, int b, int color)
+void LCDWIKI_GUI::drawBorderedRectangle(int x, int y, int w, int h, int b, int color)
 {
 	Fill_Rect(x,		y,		b,		h,	color);
 	Fill_Rect(x+w-b,	y,		b,		h,	color);
